150074a099a89308fd51285e51cad040a632bf5b
seitvalieva <sseitvalieva@gmail.com>
2024-05-23 18:02:01 +0200

    update

--- ToDo/js/script.js
+++ ToDo/js/script.js
@@ -4,10 +4,12 @@ addBtn.addEventListener('click', addTask)   // to add an eventListener to addBtn
                                             // parameteres event-type (click) and name of function to execute (addTask on click)
 
 const taskCard = document.querySelector(".todoCard")    // selecting an html element with class="todoCard"-> div with one card
-const tasksContainer = document.querySelector(".todoCards")  //selecting an html element with class="todoCards"-> div with all cards
+const tasksContainer = document.querySelector("#todoCards")  //selecting an html element with class="todoCards"-> div with all cards
 
 const delBtn = document.querySelector('.delBtn')    // select an icon trash and store in delBtn variable
-delBtn.removeEventListener('click', deleteTask)     //by clicking on the icon, the function deleteTask will delete a task
+delBtn.addEventListener('click', function() {
+    deleteTask(taskCard);
+});   //by clicking on the icon, the function deleteTask will delete a task
 
 
 // function to add a task
@@ -22,11 +24,11 @@ function addTask(){
 
     const newDelBtn = newTask.querySelector('.delBtn')  //select a trash icon of the cloned task
     newDelBtn.addEventListener('click', function(){     //add delete event listener to new task
-        deleteTask(newTask)                             // to target the new task
-    })
+        deleteTask(newTask);                             // to target the new task
+    });
 
 }
 // function to delete a task
 function deleteTask(task){
-    task.remove();
+    task.remove(); 
 }
