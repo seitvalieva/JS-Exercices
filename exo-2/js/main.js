ce549d5ed7d93e20411c1674816e7b771312d94b
seitvalieva <sseitvalieva@gmail.com>
2024-05-24 12:00:36 +0200

    styles

---
+++ exo-2/js/main.js
@@ -0,0 +1,6 @@
+const block = document.querySelector(".block")
+block.addEventListener('click', newColor())
+
+const addBlockBtn = document.querySelector(".btn")
+addBlockBtn.addEventListener('click', addblock())
+
