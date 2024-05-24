027cafc960c00e258800b349f9c93117b01c86bd
seitvalieva <sseitvalieva@gmail.com>
2024-05-24 13:56:19 +0200

    ex 2 - ftn add block

--- exo-2/js/main.js
+++ exo-2/js/main.js
@@ -1,6 +1,48 @@
-const block = document.querySelector(".block")
-block.addEventListener('click', newColor())
-
 const addBlockBtn = document.querySelector(".btn")
-addBlockBtn.addEventListener('click', addblock())
+addBlockBtn.addEventListener('click', addBlock)
 
+const block = document.querySelector(".block")
+// block.addEventListener('click', newColor())
+
+const container = document.querySelector(".container")
+
+function addBlock(){
+    const newBlock = block.cloneNode(true)
+
+    const newContent = newBlock.querySelector(".content")
+    newContent.value = "Click again"
+
+    container.appendChild(newBlock)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--------------Random color generator----------//
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
+  function setRandomColor() {
+    $("#colorpad").css("background-color", getRandomColor());
+  }
