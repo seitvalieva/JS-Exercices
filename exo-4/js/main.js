5559c62418e6f2062f88d0572f6af113a565d803
seitvalieva <sseitvalieva@gmail.com>
2024-05-27 13:25:19 +0200

    changing bg color on click

--- exo-4/js/main.js
+++ exo-4/js/main.js
@@ -1,11 +1,17 @@
 const icons = document.querySelectorAll('.icon')
 
 // console.log(icons)
+const container = document.querySelector('.container')
 
 icons.forEach((icon) => {
     icon.addEventListener("click", () => {
   
         icon.classList.toggle('animation')
+
+        const iconBgcolor = getComputedStyle(icon).backgroundColor
+        // console.log(bgcolor)
+        container.style.backgroundColor = iconBgcolor;
+
         
     })
     
